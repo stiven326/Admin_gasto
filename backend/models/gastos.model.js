c2f006aef74341773e1c5e338726b943ec389196
Stiven Ararat <ambuila@gmail.com>
2026-06-16 10:03:18 -0500

    app.js

--- backend/models/gastos.model.js
+++ backend/models/gastos.model.js
@@ -2,7 +2,7 @@ const db = require('../config/db')
 
 class GastoModel{
     static async obtenerGastos(){
-        const [rows] = await db.query('select * from transacciones')
+        const [rows] = await db.query('SELECT ct.nombre, ts.monto, ts.fecha FROM transacciones ts inner JOIN categorias ct on ts.categoria_id = ct.cat_id LIMIT 100')
         return rows
     } 
 }
