ed1ec6b972f6f7ef4fd6454f7d7562858f186dae
Stiven Ararat <ambuila@gmail.com>
2026-06-16 10:04:15 -0500

    no se que hice

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,8 +1,10 @@
 let registro = document.getElementById('transacciones');
+fetch ('http://localhost:3000/api/gastos').then(datos => datos.json()).then(registro =>{
 
-for(i=0;i<=5;i++){
-registro.innerHTML += `<div class="fila">
-                        <div>Ingreso</div>
-                        <div>20.000</div>
-                    </div>`
-}
+    for(i=0;i<=5;i++){
+    registro.innerHTML += `<div class="fila ">
+                            <div>Ingreso</div>
+                            <div>20.000</div>
+                        </div>`
+    }
+})
