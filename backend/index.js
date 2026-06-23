b62bec90583ab8b97664333aa55cf718e8f38ef9
Stiven Ararat <ambuila@gmail.com>
2026-06-23 08:09:16 -0500

    se agrego el cors

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 const router = require('./routes/router')
+const cors = require('cors')
 
+app.use(cors())
 app.listen(3000, ()=> {
     console.log('cree un servidor web')
 })
