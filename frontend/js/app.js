df4bcb1fa761a5e96e2a62186b6e0c4db3776a51
Stiven Ararat <ambuila@gmail.com>
2026-06-23 09:53:38 -0500

    se agrego codigo al app.js

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,10 +1,18 @@
-let registro = document.getElementById('transacciones');
+let transaccion = document.getElementById('transacciones');
 fetch ('http://localhost:3000/api/gastos').then(datos => datos.json()).then(registro =>{
 
     for(i=0;i<=5;i++){
-        registro.innerHTML += `<div class="fila ">
-        <div>${registro.data[i].nombre}</div>
-        <div>${registro.data[i].monto}</div>
+
+        if(registros.data[i].tipo == 1){
+            registro.innerHTML += `<div class="fila gastos">
+            <div>${registro.data[i].nombre}</div>
+            <div>${registro.data[i].monto}</div>
         </div>`
+
+         }else{
+            transaccion.innerHTML += `<div class="fila ingresos">
+            <div>${registro.data[i].nombre}</div>
+            <div>${registro.data[i].monto}</div>`
+         }
     }
 })
